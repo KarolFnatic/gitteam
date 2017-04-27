fe0b1c45d38bd5b08c8b123c8ecc4064283a8389
Karol Grabowski <grabowski-karol@wp.pl>
2017-04-27 19:59:24 +0200

    poprawione2

--- src/test/java/pl/kgrabowski/ChangeTest.java
+++ src/test/java/pl/kgrabowski/ChangeTest.java
@@ -7,21 +7,35 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by RENT on 2017-04-27.
- */
 public class ChangeTest {
     @Test
-    public void getChangeNominal_simple() throws Exception{
+    public void getChangeNominal_simple() throws Exception {
         //given
         int amount = 11;
-        int[] excpectedNominal = new int[]{10,1};
+        int[] excpectedNominal = new int[]{10, 1};
         //when
         List<Integer> changeNominal = Change.getChange(amount);
         //then
-        assertEquals(changeNominal.size(),2);
+        assertEquals(changeNominal.size(), 2);
         assertTrue(changeNominal.contains(10));
         assertTrue(changeNominal.contains(1));
+    }
+
+    @Test
+    public void getChangeNominal_simple2() throws Exception {
+        //given
+        int amount = 12;
+        int[] excpectedNominal = new int[]{10, 2};
+        //when
+        List<Integer> changeNominal = Change.getChange(amount);
+        //then
+        checkReturnNominals(excpectedNominal, changeNominal);
+    }
+    public void checkReturnNominals(int[] expected, List<Integer> returned){
+        assertEquals(expected.length, returned.size());
+        for (int expectedNominal : expected){
+            assertTrue(returned.contains(expectedNominal));
+        }
     }
 
 }
