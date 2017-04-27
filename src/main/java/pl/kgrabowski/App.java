bb2b3c6b5767057e8d6062b92b6db07ebf2b3cef
Karol Grabowski <grabowski-karol@wp.pl>
2017-04-27 18:02:24 +0200

    Reszta z dzielenia

--- src/main/java/pl/kgrabowski/App.java
+++ src/main/java/pl/kgrabowski/App.java
@@ -6,8 +6,7 @@ package pl.kgrabowski;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Heniu!" );
+    public static void main( String[] args ) {
+        System.out.println( "Wydawnie reszty w nominałach" );
     }
 }
---
+++ src/main/java/pl/kgrabowski/Change.java
@@ -0,0 +1,12 @@
+package pl.kgrabowski;
+
+import java.util.List;
+
+/**
+ * Created by RENT on 2017-04-27.
+ */
+public class Change {
+    public List<Integer> getChange(int amount){
+        return null;
+    }
+}
